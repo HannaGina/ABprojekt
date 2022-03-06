8e6e262b3fbbb81663522e6e08e6f8b5d2d83942
ViktorGyorgy <viktorgyorgy@gmail.com>
2022-03-06 16:14:52 +0200

    Elso majdnem kesz

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const net = require('net')
 const fs = require('fs');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const { fileURLToPath } = require('url');
-const { wrap } = require('module');
 const uri = "mongodb+srv://abuser:oobS68tBrelJayOp@abprojekt.4qafu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const port = 2500;
 
 function createDatabase(value){
     if (!fs.existsSync(`databases/${value}`)){
@@ -13,7 +13,7 @@ function createDatabase(value){
         return "OK";
     }
     else{
-        return "NOT OK";
+        return "Letezik mar az adatbazis.";
     }
 }
 
@@ -29,20 +29,59 @@ function dropDatabase(value){
         return 'OK';
     }
     else{
-        return 'NOT OK';
+        return 'Nem letezik az adatbazis.';
     }
 }
 
 function createTable(value){
-    let fname = `database/${value.database}/${value.tableName}.json`;
-    if(!fs.existsSync("databases/" + value.database) && !fs.existsSync(fname)){
-        fs.writeFileSync(fname, JSON.toString(value.attributes));
+    let fname = `./databases/${value.database}/${value.table}.json`;
+    let atrributeNames = value.attributes.map(a => a.name);
+    let areAtrributeNamesUnique = atrributeNames.every((e, i) => atrributeNames.indexOf(e) == i);
+    if(value.table === ''){
+        return "A tablanak kell nevet adni.";
+    }
+    else if(!areAtrributeNamesUnique){
+        return "Tobb ugyanolyan nevu attributum van.";
+    }
+    else if(atrributeNames.some(e => e == '')){
+        return "Van ures mezo.";
+    }
+    else if(value.attributes.filter(e => e.pk).length != 1){
+        return "Kell pontosan egy primary key!";
+    }
+    else if(fs.existsSync("databases/" + value.database) && !fs.existsSync(fname)){
+        fs.writeFileSync(fname, JSON.stringify(value.attributes));
         return "OK";
     }
     else{
-        return "NOT OK";
+        return "Letezik mar ez a tabla.";
     }
 }
+
+function getTables(value){
+    return fs.readdirSync(`databases/${value}/`, { withFileTypes: true })
+        .map(file => file.name.replace(".json", ""))
+}
+
+function dropTable(value){
+    let fname = `databases/${value.database}/${value.table}.json`;
+    if(fs.existsSync(fname)){
+        fs.rmSync(fname);
+        return "OK";
+    }
+    else{
+        return "A tabla nem letezik.";
+    }
+}
+
+function getAttributesByType(value){
+    if(value.table === ''){
+        return '';
+    }
+    var attributes = require(`./databases/${value.database}/${value.table}.json`);
+    return attributes.filter(a => a.type === value.type).map(a => a.name).toString()
+}
+
 
 
 const server = net.createServer((socket) => {
@@ -64,28 +103,29 @@ const server = net.createServer((socket) => {
             case "Create Table":
                 answer = createTable(data.value);
                 break;
+            case "Get Tables":
+                answer = getTables(data.value);
+                break;
+            case "Drop Table":
+                answer = dropTable(data.value);
+                break;
+            case "Get Attributes By Type":
+                answer = getAttributesByType(data.value);
+                break;
         }
 
         socket.write(answer + '\n');
         socket.pipe(socket);
         socket.destroy();
     });
-
-    
 })
 
 
-server.listen(2500, () =>{
+server.listen(port, () =>{
+    console.log(`Server is listening on http://localhost:${port}`)
     client.connect()
 })
 
 server.on('close', () =>{
     client.close();
 })
-
-async function hello(){
-    let y = getDatabases();
-    console.log(y);
-}
-
-hello();
